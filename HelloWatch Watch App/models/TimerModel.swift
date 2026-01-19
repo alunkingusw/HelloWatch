9c385a23246aa10961e17d27fcc724a7d2a48c01
Alun King <alun.king1@southwales.ac.uk>
2026-01-19 13:19:14 +0000

    Added some documentation to the TimerModel class.

--- HelloWatch Watch App/models/TimerModel.swift
+++ HelloWatch Watch App/models/TimerModel.swift
@@ -9,19 +9,36 @@ import SwiftUI
 class TimerModel {
     
     var timerDetails: TimerDetails = TimerDetails()
+    /* the question mark means that the variable is an 'optional'
+     * optional values can be null or not null, we are not sure
+     * so we mark them with a question mark.
+     * When we want to use them, we have to unwrap them, as shown below (line 33 / 36)
+     */
     var timer: Timer?
     
     
     func start(){
         if timerDetails.isRunning == false{
+            timerDetails.isRunning = true
             timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats:true) {_ in
                 self.timerDetails.timeElapsed += 0.01
             }
+            
         }
     }
+    
     func stop(){
         if timerDetails.isRunning{
+            
+            //one way of accessing timer
             timer?.invalidate()
+            
+            //another way
+            if let timerUnwrapped = timer{
+                timerUnwrapped.invalidate()
+            }
+            
+            timerDetails.isRunning = false
         }
     }
     
